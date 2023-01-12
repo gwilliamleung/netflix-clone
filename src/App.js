7bd0c8de36bdddd993c6e1f19a229b117ef3c2c1
William Leung <gwilliamleung@gmail.com>
2023-01-12 16:11:26 -0500

    base project complete

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import "./index.css"
+import { AuthContextProvider } from "./context/AuthContext";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Account from "./pages/Account";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <AuthContextProvider>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signUp" element={<Signup />} />
+        <Route path="/account" element={<ProtectedRoute><Account /></ProtectedRoute>}/>
+      </Routes>
+    </AuthContextProvider>
     </>
   );
 }
